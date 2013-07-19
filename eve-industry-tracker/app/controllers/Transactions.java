3b5802e0b1d153004a2b2cce5be8b028e8975ed2
Carl Tompkins <cl05tomp@gmail.com>
2013-07-18 21:32:09 -0400

    Added cached db schema, code to grab wallet transactions from API

--- eve-industry-tracker/app/controllers/Transactions.java
+++ eve-industry-tracker/app/controllers/Transactions.java
@@ -1,12 +1,44 @@
 package controllers;
 
+import java.util.Set;
+
+import com.beimin.eveapi.account.characters.CharactersParser;
+import com.beimin.eveapi.account.characters.CharactersResponse;
+import com.beimin.eveapi.account.characters.EveCharacter;
+import com.beimin.eveapi.character.wallet.transactions.WalletTransactionsParser;
+import com.beimin.eveapi.core.ApiAuthorization;
+import com.beimin.eveapi.exception.ApiException;
+import com.beimin.eveapi.shared.wallet.transactions.ApiWalletTransaction;
+import com.beimin.eveapi.shared.wallet.transactions.WalletTransactionsResponse;
+
 import play.*;
 import play.mvc.*;
 
 import views.html.transactions.*;
 
 public class Transactions extends Controller {
-	public static Result viewTransactions() {
+	
+	public void getTransactions() throws ApiException {
+		int keyCode = 2382842;
+		String vCode = "BUJPu4zQcZs49AIWpcs5CTD4hwOUUCra9ZuGFsfC2OlaCIktec3jyOCgjro841GG";
+		ApiAuthorization auth = new ApiAuthorization(keyCode,vCode);
+		CharactersParser parser = CharactersParser.getInstance();
+		CharactersResponse response = parser.getResponse(auth);
+		Set<EveCharacter> characters = response.getAll();
+		for (EveCharacter character : characters) {
+			System.out.println(character.getCharacterID());
+		}
+		WalletTransactionsParser tParser = WalletTransactionsParser.getInstance();
+			WalletTransactionsResponse tResponse = tParser.getResponse(auth, keyCode);
+			System.out.println(tResponse.getCachedUntil());
+			for (ApiWalletTransaction t : tResponse.getAll()) {
+				System.out.println(t.getPrice());
+			}
+	}
+	
+	public static Result viewTransactions() throws ApiException {
+		Transactions t = new Transactions();
+		t.getTransactions();
         return ok(summary.render());
     }
 }
