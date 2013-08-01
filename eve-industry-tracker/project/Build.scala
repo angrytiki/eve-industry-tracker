a49ca8e9d6f4a3dfe0fadfd40e0f90ab18fd8483
Charles H <chathaway@logrit.com>
2013-07-31 21:24:30 -0400

    Copied in the angular-seed-play files

--- eve-industry-tracker/project/Build.scala
+++ eve-industry-tracker/project/Build.scala
@@ -1,24 +1,15 @@
 import sbt._
-import Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "eve-industry-tracker"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "angular-seed-play"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean,
-    "org.webjars" %% "webjars-play" % "2.1.0-2",
     "org.webjars" % "angularjs" % "1.1.5-1",
-    "org.webjars" % "bootstrap" % "2.3.2"
-  )
+    "org.webjars" % "requirejs" % "2.1.1",
+    "org.webjars" % "webjars-play" % "2.1.0-1")
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
-
+  val main = play.Project(appName, appVersion, appDependencies)
 }
---
+++ eve-industry-tracker/public/javascripts/main.js
@@ -0,0 +1,4 @@
+define(["webjars!jquery.js"], function() {
+  // $ can be accessed within here as jquery.js declares it as a global.
+  console.log("Here");
+});
